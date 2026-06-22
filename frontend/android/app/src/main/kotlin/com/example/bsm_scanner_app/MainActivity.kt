62fb364809c22687d6bad88111d77807f7d75822
TudowTwelvvy <tumelokhanye9811@gmail.com>
2026-06-22 12:41:36 +0200

    deleted the frontend folder and added  bsm_app

--- frontend/android/app/src/main/kotlin/com/example/bsm_scanner_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bsm_scanner_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/lib/core/constants/firestore_paths.dart
+++
@@ -1,6 +0,0 @@
-class FirestorePaths {
-  static const String users = 'users';
-  static const String products = 'products';
-
-  static String userProducts(String userId) => 'users/$userId/$products';
-}
